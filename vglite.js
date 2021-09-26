a27b0781de18f3f8baa830e6a9b141145d426960
Liam Paternott <38994791+lpat0002@users.noreply.github.com>
2021-09-26 20:07:39 +1000

    Homework files added - for the site

---
+++ vglite.js
@@ -0,0 +1,6 @@
+var vg_1 = "W9_HW_MapVis.vega";
+vegaEmbed("#chmap", vg_1).then(function(result) {
+
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
